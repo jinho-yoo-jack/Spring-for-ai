940aadb27b44f4892b19ecfe57ddc27ee6c55d43
jinho-yoo-jack <jhy7342@gmail.com>
2025-11-06 23:04:42 +0900

    INIT: Project

---
+++ src/test/java/org/sprain/ai/SprainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sprain.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
